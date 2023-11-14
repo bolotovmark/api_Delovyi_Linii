54bdf3c70f05c55b1b70a2e97edf7fc2daacdbac
markbolotov <bolmark2003@gmail.com>
2023-11-14 19:10:06 +0500

    1

--- api_Delovyi_Linii/Program.cs
+++ api_Delovyi_Linii/Program.cs
@@ -6,4 +6,5 @@ var client = new ClientDelovyiLinii("0F74D344-4281-4A25-A545-8A5A48566D44");
 Console.WriteLine(await client.CalculatePrice("Пермь, Профессора Дедюкина 18", 12, 0.07));
 //Неполный адресс
 Console.WriteLine(await client.CalculatePrice("Пермь", 12, 0.07));
+
 //Console.WriteLine(await client.GetFirstDate("г. Чайковский, ул. Промышленная, 8/25", 1, 0.1)); 
